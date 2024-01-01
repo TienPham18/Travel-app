6a41ae4cd541da4ee58546027975f787634b422a
Tien Pham <tienphamvanfuv@gmail.com>
2024-01-01 12:47:09 +0100

    add CampProps

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage}`}>
+      Camp Site 1
+    </div>
+  );
+};
+
 export default function Camp() {
   return (
     <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340] w-full items-start justify-start gap-8 overflow-x-auto lg:h]400px] xl:h-[640px]"></div>
+      <div className="hide-scrollbar flex h-[340] w-full items-start justify-start gap-8 overflow-x-auto lg:h]400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+      </div>
     </section>
   );
 }
