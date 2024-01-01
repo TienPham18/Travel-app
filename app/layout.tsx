7b0ac8904713699e8c4bd178a5a045d75613329d
Tien Pham <tienphamvanfuv@gmail.com>
2024-01-01 19:05:28 +0100

    add media query

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overlow-hidden">{children}</main>
+        <main className="relative overflow-hidden">{children}</main>
         <Footer />
       </body>
     </html>
