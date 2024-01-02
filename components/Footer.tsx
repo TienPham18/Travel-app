3d20c14b416e63a00fc66f0b66157b5f1f5289d9
Tien Pham <tienphamvanfuv@gmail.com>
2024-01-02 13:24:19 +0100

    Create FooterColumnProps

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,40 @@
+import { FOOTER_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="/hilink-logo.svg" width={75} height={29} alt="Logo" />
+          </Link>
+
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30"></ul>
+              </FooterColumn>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h4>{title}</h4>
+      {children}
+    </div>
+  );
+};
