d36553349d0e9e2126711b4a6e7d6bf7720bf13d
Tien Pham <tienphamvanfuv@gmail.com>
2024-01-02 13:39:57 +0100

    add contactInfo and socialMediaIcons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FOOTER_LINKS } from "@/constants";
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -15,9 +15,50 @@ export default function Footer() {
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns) => (
               <FooterColumn title={columns.title}>
-                <ul className="regular-14 flex flex-col gap-4 text-gray-30"></ul>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
               </FooterColumn>
             ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link
+                    href="/"
+                    key={link.label}
+                    className="flex gap-4 md:flex-col lg:flew-row"
+                  >
+                    <p className="whitespace-nowrap">{link.label}</p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
+                    </p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4">
+                  {SOCIALS.links.map((link) => (
+                    <Link href="/" key={link}>
+                      <Image
+                        src={link}
+                        alt="media logo"
+                        width={24}
+                        height={24}
+                        className="cursor-pointer"
+                      />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
           </div>
         </div>
       </div>
@@ -33,7 +74,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-4">
-      <h4>{title}</h4>
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
       {children}
     </div>
   );
